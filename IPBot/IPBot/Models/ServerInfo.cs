0ef514567ce247bfdd04bdabbc30c3986d56bb12
Sean Edwards <hello@seanedwards.dev>
2022-04-09 15:16:05 +0100

    Add default Online value

--- IPBot/IPBot/Models/ServerInfo.cs
+++ IPBot/IPBot/Models/ServerInfo.cs
@@ -2,7 +2,7 @@
 {
     internal record ServerInfo
     {
-        public bool Online { get; }
+        public bool Online { get; } = false;
         public string Map { get; }
         public int PlayerCount { get; }
         public List<string> PlayerNames { get; }
