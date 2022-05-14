4e21f5f3d394929e84256979846c4905c45fc729
Sean Edwards <hello@seanedwards.dev>
2022-05-14 11:59:14 +0100

    Use Newtonsoft.Json for Json convert

--- IPBot/IPBot/Models/ServerInfo.cs
+++ IPBot/IPBot/Models/ServerInfo.cs
@@ -1,19 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿namespace IPBot.Models;
 
-namespace IPBot.Models
+internal record ServerInfo
 {
-    internal class ServerInfo
-    {
-        [JsonPropertyName("online")]
-        public bool Online { get; set; } = false;
+    [JsonProperty("online")]
+    public bool Online { get; set; }
 
-        [JsonPropertyName("map")]
-        public string Map { get; set; }
+    [JsonProperty("map")]
+    public string Map { get; set; }
 
-        [JsonPropertyName("player_count")]
-        public int PlayerCount { get; set; }
+    [JsonProperty("player_count")]
+    public int PlayerCount { get; set; }
 
-        [JsonPropertyName("player_names")]
-        public List<string> PlayerNames { get; set; }
-    }
+    [JsonProperty("player_names")]
+    public List<string> PlayerNames { get; set; }
 }
