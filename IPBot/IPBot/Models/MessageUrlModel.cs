4ac44c49b05c8e098d591c8bf107fea554134722
Sean Edwards <hello@seanedwards.dev>
2022-05-18 13:14:35 +0100

    Check if message contains URL

---
+++ IPBot/IPBot/Models/MessageUrlModel.cs
@@ -0,0 +1,7 @@
+namespace IPBot.Models;
+
+public class MessageUrlModel
+{
+    public bool Success { get; set; }
+    public string Url { get; set; }
+}
--- IPBot/IPBot/Services/MessageAnalyserService.cs
+++ IPBot/IPBot/Services/MessageAnalyserService.cs
@@ -1,25 +1,32 @@
 ﻿using System.Text.RegularExpressions;
 using IPBot.Helpers;
+using IPBot.Models;
 
 namespace IPBot.Services;
 
 internal class MessageAnalyserService
 {
     private static readonly AnimeAnalyser.AnimeAnalyser AnimeAnalyser = new();
-    
     private static readonly List<string> ResponseList = new()
     {
         "https://c.tenor.com/xwvZutw8Z7AAAAAC/tenor.gif",
         "https://64.media.tumblr.com/c045b0be831f9a3eaff6ef009d182f03/tumblr_mh958xh2jX1qa8a12o1_500.gif",
         "https://i2.wp.com/www.nerdsandbeyond.com/wp-content/uploads/2020/08/SmoggyHilariousBaiji-size_restricted.gif?resize=540%2C304",
-        "https://c.tenor.com/NDe_7Jj8RaEAAAAd/tenor.gif"
+        "https://c.tenor.com/NDe_7Jj8RaEAAAAd/tenor.gif",
+        "https://tenor.com/view/unfunny-meme-citation-moa-a-citation-warning-issued-no-penalty-gif-16450177",
+        "https://64.media.tumblr.com/2714da2b6d92e94bc21654b2d6e2a239/tumblr_mq3rkbHSDG1szkv9io1_500.gif",
+        "https://gfycat.com/unhappyfatherlygoa",
+        "https://c.tenor.com/JsV3NsOZPF4AAAAd/tenor.gif",
+        "https://media.discordapp.net/attachments/140558319340355584/976269413047226378/Untitled.gif",
+        "https://i.imgur.com/R6qrD.gif",
+        "https://64.media.tumblr.com/tumblr_lv2kv1n4OU1r4zr2vo2_r4_500.gif"
     };
-
+    
     public static async Task CheckMessageForAnimeAsync(SocketMessage message)
     {
-        var channelName = DebugHelper.IsDebug() ? "anti-anime-test" : "the-gospel";
+        var channelNames = DebugHelper.IsDebug() ? new List<string> { "anti-anime-test" } : new List<string> { "anti-anime-test", "the-gospel" };
 
-        if (message.Channel.Name == channelName && !message.Author.IsBot)
+        if (channelNames.Contains(message.Channel.Name) && !message.Author.IsBot)
         {
             if (await MessageContainsAnimeAsync(message))
             {
@@ -28,47 +35,47 @@ internal class MessageAnalyserService
             }
         }
     }
-    
+
     private static async Task<bool> MessageContainsAnimeAsync(SocketMessage message)
     {
         if (!string.IsNullOrEmpty(message.Content))
         {
-            double animeScore;
+            var messageUrlModel = MessageContainsUrl(message.Content);
+            if (messageUrlModel.Success)
+            {
+                double animeScore;
 
-            var youtubeRegex =
-                new Regex(
-                    @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$");
-            var isYouTubeUrl = youtubeRegex.Match(message.Content);
-                
-            if (isYouTubeUrl.Success)
-            {
-                var watchId = isYouTubeUrl.Groups.Values.ToArray()[6];
-                var url = $"https://i.ytimg.com/vi/{watchId}/sddefault.jpg";
-                    
-                animeScore = await GetAnimeScoreAsync(url);
-            }
-            else
-            {
-                var imageFormats = new List<string>
+                var youtubeUrlModel = MessageContainsYouTubeLink(message.Content);
+                if (youtubeUrlModel.Success)
                 {
-                    ".png",
-                    ".jpeg",
-                    ".jpg",
-                    ".tiff"
-                };
-                
-                var url = message.Content;
-                if(!imageFormats.Contains(message.Content[new Range(message.Content.Length - 4, message.Content.Length)]) )
-                {
-                    url = message.Content + ".gif";
+                    var url = $"https://i.ytimg.com/vi/{youtubeUrlModel.Url}/sddefault.jpg";
+
+                    animeScore = await GetAnimeScoreAsync(url);
                 }
-                
-                animeScore = await GetAnimeScoreAsync(url);
-            }
+                else
+                {
+                    var imageFormats = new List<string>
+                    {
+                        ".png",
+                        ".jpeg",
+                        ".jpg",
+                        ".tiff",
+                        ".mp4"
+                    };
 
-            if (animeScore > 0)
-            {
-                return true;
+                    var url = messageUrlModel.Url;
+                    if (!imageFormats.Any(x => message.Content.Contains(x)))
+                    {
+                        url = message.Content + ".gif";
+                    }
+
+                    animeScore = await GetAnimeScoreAsync(url);
+                }
+
+                if (animeScore > 0)
+                {
+                    return true;
+                }
             }
         }
 
@@ -90,5 +97,37 @@ internal class MessageAnalyserService
     private static async Task<double> GetAnimeScoreAsync(string url)
     {
         return await AnimeAnalyser.GetAnimeScoreAsync(url);
+    }
+
+    private static MessageUrlModel MessageContainsUrl(string messageContent)
+    {
+        foreach (var word in messageContent.Split())
+        {
+            var urlMatch = Regex.Match(word, @"^https?:\/\/(?:www\\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$");
+
+            if (!urlMatch.Success) continue;
+
+            return new MessageUrlModel
+            {
+                Success = true,
+                Url = urlMatch.Success ? urlMatch.Value : string.Empty
+            };
+        }
+
+        return new MessageUrlModel
+        {
+            Success = false
+        };
+    }
+
+    private static MessageUrlModel MessageContainsYouTubeLink(string messageContent)
+    {
+        var youtubeUrlMatch = Regex.Match(messageContent, @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$");
+
+        return new MessageUrlModel
+        {
+            Success = youtubeUrlMatch.Success,
+            Url = youtubeUrlMatch.Success ? youtubeUrlMatch.Groups.Values.ToList()[6].ToString() : string.Empty
+        };
     }
 }
