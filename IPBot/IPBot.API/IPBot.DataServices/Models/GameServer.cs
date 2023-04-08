49a08565558c1d03f1d12cbef4ed14bf5a490602
Sean Edwards <hello@seanedwards.dev>
2023-04-08 17:18:16 +0100

    Fix formatting

--- IPBot/IPBot.API/IPBot.DataServices/Models/GameServer.cs
+++ IPBot/IPBot.API/IPBot.DataServices/Models/GameServer.cs
@@ -5,8 +5,8 @@ public class GameServer
 	public int Id { get; set; }
 	public int Port { get; set; }
 	public bool Active { get; set; }
-	public string Map { get; set; }
-    public int GameId { get; set; }
+	public string Map { get; set; } 
+	public int GameId { get; set; }
 
 	public virtual Game Game { get; set; }
 }
--- IPBot/IPBot.DTOs/Dtos/GameServerDto.cs
+++ IPBot/IPBot.DTOs/Dtos/GameServerDto.cs
@@ -5,7 +5,7 @@ public class GameServerDto
 	public int Id { get; set; }
 	public int Port { get; set; }
 	public bool Active { get; set; }
-	public string Map { get; set; }
-    public int GameId { get; set; }
+	public string Map { get; set; } 
+	public int GameId { get; set; }
 }
 
