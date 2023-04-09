5f46112a4882ecbcb1451331c025bae2da20d42c
Sean Edwards <hello@seanedwards.dev>
2023-04-09 13:23:15 +0100

    Remove unused constant

--- IPBot/IPBot/IPBot/BotConstants.cs
+++ IPBot/IPBot/IPBot/BotConstants.cs
@@ -4,7 +4,6 @@ public static class BotConstants
 {
     public const int MinecraftServerPort = 25565;
     public const int ZomboidServerPort = 16261;
-    public static readonly string ConfigDirectory = AppContext.BaseDirectory + "Configs";
     public const string ServerOfflineString = "The server is currently offline :(";
     public const string SeverOnlineString = "The server is online!";
     public const double AnimeScoreTolerance = 0.75;
