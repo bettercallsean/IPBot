132bf406d9c73ce4bba55f7221feb499ee75a1e2
Sean Edwards <hello@seanedwards.dev>
2024-03-12 13:47:37 +0000

    Move all files and folders up a level

---
+++ IPBot/IPBot/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Text;
+global using Discord.Interactions;
+global using Discord.WebSocket;
+global using IPBot.Models;
+global using Microsoft.Extensions.Configuration;
+global using Newtonsoft.Json;
--- publish_containers.ps1
+++ publish_containers.ps1
@@ -1,14 +1,14 @@
 $scriptDirectory = Get-Location
 
-Set-Location "../IPBot/IPBot"
-dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release
+Set-Location "../IPBot"
+dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
 docker tag ipbot raspberrypi.local:9298/ipbot
 docker push raspberrypi.local:9298/ipbot
 
 Set-Location $scriptDirectory
 
-Set-Location "../IPBot/IPBot.API"
-dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release
+Set-Location "../IPBot.API"
+dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
 docker tag ipbot-api raspberrypi.local:9298/ipbot-api
 docker push raspberrypi.local:9298/ipbot-api
 
