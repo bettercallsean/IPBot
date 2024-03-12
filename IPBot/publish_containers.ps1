5c05859aadb3f08a6f1094331a2753326ee46272
Sean Edwards <hello@seanedwards.dev>
2024-03-12 12:06:43 +0000

    Add publish_containers script

---
+++ IPBot/publish_containers.ps1
@@ -0,0 +1,15 @@
+$scriptDirectory = Get-Location
+
+Set-Location "../IPBot/IPBot"
+dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release
+docker tag ipbot raspberrypi.local:9298/ipbot
+docker push raspberrypi.local:9298/ipbot
+
+Set-Location $scriptDirectory
+
+Set-Location "../IPBot/IPBot.API"
+dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release
+docker tag ipbot-api raspberrypi.local:9298/ipbot-api
+docker push raspberrypi.local:9298/ipbot-api
+
+ssh pi 'cd ~/src/IPBot && docker compose pull && docker-compose up -d --no-build'
