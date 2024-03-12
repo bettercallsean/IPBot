84f3721b878784b718f41349c503515370b65673
Sean Edwards <hello@seanedwards.dev>
2024-03-12 13:55:12 +0000

    Update Set-Location path

--- publish_containers.ps1
+++ publish_containers.ps1
@@ -1,13 +1,13 @@
 $scriptDirectory = Get-Location
 
-Set-Location "../IPBot"
+Set-Location "IPBot"
 dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
 docker tag ipbot raspberrypi.local:9298/ipbot
 docker push raspberrypi.local:9298/ipbot
 
 Set-Location $scriptDirectory
 
-Set-Location "../IPBot.API"
+Set-Location "IPBot.API"
 dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
 docker tag ipbot-api raspberrypi.local:9298/ipbot-api
 docker push raspberrypi.local:9298/ipbot-api
