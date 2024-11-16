570bdbed85ce235db2a6be8ce89fac3b12038219
Sean Edwards <hello@seanedwards.dev>
2024-11-16 21:24:41 +0000

    Make class static

--- IPBot.API/IPBot.API/Constants/KeyedHttpClientNames.cs
+++ IPBot.API/IPBot.API/Constants/KeyedHttpClientNames.cs
@@ -1,6 +1,6 @@
 ﻿namespace IPBot.API.Constants;
 
-public class KeyedHttpClientNames
+public static class KeyedHttpClientNames
 {
     public const string LocalIPClient = "Local IP Client";
     public const string MinecraftServerClient = "Minecraft Server Client";
