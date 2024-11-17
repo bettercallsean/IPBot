0b36946108b72904aec3e098d57fb1bec8945686
Sean Edwards <hello@seanedwards.dev>
2024-11-17 18:19:14 +0000

    Use Push-Container function to push containers

--- publish_containers.ps1
+++ publish_containers.ps1
@@ -2,14 +2,12 @@ $scriptDirectory = Get-Location
 
 Set-Location "IPBot"
 dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-docker tag ipbot raspberrypi.local:9298/ipbot
-docker push raspberrypi.local:9298/ipbot
+Push-Container("ipbot")
 
 Set-Location $scriptDirectory
 
 Set-Location "IPBot.API"
 dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-docker tag ipbot-api raspberrypi.local:9298/ipbot-api
-docker push raspberrypi.local:9298/ipbot-api
+Push-Container("ipbot-api")
 
 ssh pi 'cd ~/src/IPBot && docker compose pull && docker-compose up -d --no-build'
