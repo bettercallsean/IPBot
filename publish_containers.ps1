48ff4d68eed35b2d1e91d0a7188872682d64e518
Sean Edwards <hello@seanedwards.dev>
2024-11-17 19:25:17 +0000

    Move publishing code to global function

--- publish_containers.ps1
+++ publish_containers.ps1
@@ -1,13 +1,11 @@
 $scriptDirectory = Get-Location
 
 Set-Location "IPBot"
-dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-Push-Container("ipbot")
+Publish-Container "ipbot"
 
 Set-Location $scriptDirectory
 
 Set-Location "IPBot.API"
-dotnet publish --os linux --arch arm64 /t:PublishContainer -c Release -p:ContainerFamily=jammy-chiseled
-Push-Container("ipbot-api")
+Publish-Container "ipbot-api"
 
 ssh pi 'cd ~/src/IPBot && docker compose pull && docker-compose up -d --no-build'
