eead1e66ff705c094ff41f187e0e3796c112921f
Sean Edwards <hello@seanedwards.dev>
2025-01-26 12:41:21 +0000

    Use init keyword

--- IPBot.API/IPBot.API/Configuration/AzureSettings.cs
+++ IPBot.API/IPBot.API/Configuration/AzureSettings.cs
@@ -2,6 +2,6 @@ namespace IPBot.API.Configuration;
 
 public class AzureSettings
 {
-    public string SubscriptionKey { get; set; }
-    public string Endpoint { get; set; }
+    public string SubscriptionKey { get; init; }
+    public string Endpoint { get; init; }
 }
